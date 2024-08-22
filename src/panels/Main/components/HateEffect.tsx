23f8eed3aad194eb5ac7871adb157ae45dfb68b7
Color-Kat <katik7979@gmail.com>
2024-08-22 21:23:10 +0300

    super-puper hate effect improvement

--- src/panels/Main/components/HateEffect.tsx
+++ src/panels/Main/components/HateEffect.tsx
@@ -33,11 +33,7 @@ const ParticlesEffect = () => {
                 duration  : 500,
                 particles : {
                     number: {
-                        value  : 40, // Particles count
-                        density: {
-                            enable    : true,
-                            value_area: 400,
-                        },
+                        value  : 50, // Particles count
                     },
                     //  Image is a particle
                     shape  : {
@@ -56,7 +52,7 @@ const ParticlesEffect = () => {
                     },
                     move   : {
                         enable   : true,
-                        speed    : 5, // Move speed
+                        speed    : 16, // Move speed
                         vibrate  : true,
                         direction: "outside", // Move direction
                         out_mode : "out",
@@ -108,7 +104,7 @@ export const HateEffect: FC<HateEffectProps> = memo(({ show }) => {
             className={twJoin(
                 "hate-effect",
                 "absolute right-0 left-0 z-[1]",
-                'flex transition-all duration-300 ',
+                'flex transition-all duration-300 will-change-auto',
                 show ? "top-0" : "-top-full" // Show the effect when show is true
             )}
         >
@@ -116,7 +112,7 @@ export const HateEffect: FC<HateEffectProps> = memo(({ show }) => {
             <img
                 className={twJoin(
                     "w-72 h-auto absolute -top-10 -left-16 rotate-[151deg] blur-[0.7px] z-10",
-                    "transition-transform duration-500 ease-out",
+                    "transition-transform duration-500 ease-out will-change-transform",
                     show ? "translate-y-0 translate-x-0" : "-translate-y-full -translate-x-full" // Additional animation
                 )}
                 src="/feed/hateCat.png"
@@ -127,7 +123,7 @@ export const HateEffect: FC<HateEffectProps> = memo(({ show }) => {
             <div
                 className={twJoin(
                     "mr-6 mt-16 ml-auto relative z-20",
-                    "transition-transform duration-500 ease-in-out",
+                    "transition-transform duration-500 ease-in-out will-change-transform",
                     show ? "translate-y-0 translate-x-0" : "-translate-y-full" // Additional animation
                 )}
             >
